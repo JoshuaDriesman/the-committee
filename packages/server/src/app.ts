dfdd21798e20f0e09857ec93af3caa3f3ed46de2
Joshua Driesman <jdriesman@outlook.com>
2019-02-17 23:16:51 -0500

    Got basic express and mongoose stuff working

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from "express";
+import mongoose, { mongo } from "mongoose";
+
+const dbConn = mongoose.connect("mongodb://localhost/comm-test");
+const schema = new mongoose.Schema({
+  time: Number,
+});
+const Model = mongoose.model("Model", schema);
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req: Request, res: Response) => {
+  const stuff = new Model({ time: Date.now() });
+  stuff.save();
   res.send("Hello World!");
 });
 
